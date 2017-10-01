cccc3ba2e1aca2c7aa866d1ac035fd00ed938c51
MariaSyed <mariasy@metropolia.fi>
2017-10-01 09:21:59 +0300

    phase buttons

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,55 +1,75 @@
-import React from 'react'
+import React, { Component } from 'react'
 import NavBar from '../components/NavBar'
 import { Grid, Row, Col } from 'react-bootstrap'
+import { Redirect } from 'react-router-dom'
 
-const Home = () => {
-  return (
-    <div>
-      <NavBar />
-      <div style={styles.carouselDiv}>
-        <div style={styles.centeredDiv}>
-          <p style={styles.subheading}>Innovation Space for You</p>
-          <p style={styles.mainHeading}>INNOV8</p>
+class Home extends Component {
+  state = {
+    redirectTo: null
+  }
+
+  render() {
+    if (this.state.redirectTo) {
+      return <Redirect to={this.state.redirectTo} />
+    }
+    return (
+      <div>
+        <NavBar />
+        <div style={styles.carouselDiv}>
+          <div style={styles.centeredDiv}>
+            <p style={styles.subheading}>Innovation Space for You</p>
+            <p style={styles.mainHeading}>INNOV8</p>
+          </div>
+        </div>
+
+        <div>
+          <Grid style={styles.grid}>
+            <Row>
+              <Col md={6} style={styles.leftGrid}>
+                <p style={styles.leftText}>Organise.</p>
+                <p style={styles.leftText}>Mentor.</p>
+                <p style={styles.leftText}>Solve.</p>
+              </Col>
+              <Col md={6} style={styles.rightGrid}>
+                <p style={styles.rightTop}>WHAT?</p>
+                <p style={styles.rightText}>Find a challenge</p>
+                <p style={styles.rightText}>Organise a hackathon</p>
+                <p style={styles.rightText}>Book a space</p>
+                <p style={styles.rightText}>Mentor it!</p>
+                <div style={styles.rightButtonContainer}>
+                  <button style={styles.rightButton}>LEARN MORE</button>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={4} style={{ width: '100%', paddingTop: 100, height: '65vh', backgroundColor: '#000' }}>
+                <p style={styles.phaseTitle}>PHASE 1</p>
+                <p style={styles.phaseSubTitle}>Create a Challenge</p>
+                <div style={styles.phaseButtonContainer}>
+                  <button onClick={() => this.setState({ redirectTo: '/post-challenge' })} style={Object.assign({}, styles.phaseButton, { color: '#000' })}>Create</button>
+                </div>
+              </Col>
+              <Col md={4} style={{ width: '100%', paddingTop: 100, height: '65vh', backgroundColor: '#5e636a' }}>
+                <p style={styles.phaseTitle}>PHASE 2</p>
+                <p style={styles.phaseSubTitle}>Organize a Hackathon</p>
+                <div style={styles.phaseButtonContainer}>
+                  <button onClick={() => this.setState({ redirectTo: '/challenges' })} style={Object.assign({}, styles.phaseButton, { color: '#000' })}>Organize</button>
+                </div>
+              </Col>
+              <Col md={4} style={{ width: '100%', paddingTop: 100, height: '65vh', backgroundColor: 'white' }}>
+                <p style={Object.assign({},styles.phaseTitle, { color: 'black'})}>PHASE 3</p>
+                <p style={Object.assign({},styles.phaseSubTitle, { color: 'black'})}>Implement your Solution</p>
+                <div style={styles.phaseButtonContainer}>
+                  <button onClick={() => {}} style={Object.assign({}, styles.phaseButton, { backgroundColor: '#000'} )}>Implement</button>
+                </div>
+              </Col>
+            </Row>
+          </Grid>
         </div>
       </div>
+    )
+  }
 
-      <div>
-        <Grid style={styles.grid}>
-          <Row>
-            <Col md={6} style={styles.leftGrid}>
-              <p style={styles.leftText}>Organise.</p>
-              <p style={styles.leftText}>Mentor.</p>
-              <p style={styles.leftText}>Solve.</p>
-            </Col>
-            <Col md={6} style={styles.rightGrid}>
-              <p style={styles.rightTop}>WHAT?</p>
-              <p style={styles.rightText}>Find a challenge</p>
-              <p style={styles.rightText}>Organise a hackathon</p>
-              <p style={styles.rightText}>Book a space</p>
-              <p style={styles.rightText}>Mentor it!</p>
-              <div style={styles.rightButtonContainer}>
-                <button style={styles.rightButton}>LEARN MORE</button>
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={4} style={{ paddingTop: 100, height: '65vh', backgroundColor: '#000' }}>
-              <p style={styles.phaseTitle}>PHASE 1</p>
-              <p style={styles.phaseSubTitle}>Create a Challenge</p>
-            </Col>
-            <Col md={4} style={{ paddingTop: 100, height: '65vh', backgroundColor: '#5e636a' }}>
-              <p style={styles.phaseTitle}>PHASE 2</p>
-              <p style={styles.phaseSubTitle}>Organize a Hackathon</p>
-            </Col>
-            <Col md={4} style={{ paddingTop: 100, height: '65vh', backgroundColor: 'white' }}>
-              <p style={Object.assign({},styles.phaseTitle, { color: 'black'})}>PHASE 3</p>
-              <p style={Object.assign({},styles.phaseSubTitle, { color: 'black'})}>Implement your Solution</p>
-            </Col>
-          </Row>
-        </Grid>
-      </div>
-    </div>
-  )
 }
 
 const styles = {
@@ -96,11 +116,13 @@ const styles = {
     height: 600,
     backgroundColor: '#f9f907',
     paddingTop: 100,
-    paddingRight: 100
+    paddingRight: 100,
+    width: '100%',
   },
   rightGrid: {
     height: 600,
-    backgroundColor: 'black'
+    backgroundColor: 'black',
+    width: '100%',
   },
   rightTop: {
     color: 'white',
@@ -153,9 +175,28 @@ const styles = {
     textAlign: 'center'
   },
   phaseButton: {
+    fontSize: 18,
+    backgroundColor: '#fff',
     textDecoration: 'none',
+    paddingLeft: 50,
+    paddingRight: 50,
+    paddingTop: 30,
+    paddingTop: 20,
+    paddingBottom: 20,
+    color: 'white',
+    fontFamily: 'Avenir',
     border:'1px solid transparent',
-    boxRadius: 25
+    cursor: 'pointer',
+    marginTop: 20,
+    fontSize: 18,
+    marginBottom: 20,
+    borderRadius: 30
+  },
+  phaseButtonContainer: {
+    width: '30%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 60
   }
 }
 
